6a5540dd14f492cbc3d4bdce70e9298aff6d0844
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2019-10-24 20:47:55 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,26 +5,46 @@ import json
 ServerIP = "145.24.222.103"
 Port = 8001
 
-class Main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ServerIP, Port))
-    msgraw = s.recv(1024)
-    msg = msgraw.decode("utf-8")
-    print(msg)
-    if "connection" in msg:
-        reply = {}
-        reply["studentnr"] = "123456"
-        reply["classname"] = "INF2C"
-        reply["clientid"] = 1
-        reply["teamname"] = "Gerrie en Timmie"
-        reply["ip"] = socket.gethostbyname(socket.gethostname())
-        reply["secret"] = None
-        reply["status"] = None
-        replyserialized = json.dumps(reply)
-        s.send(bytes(replyserialized, "utf-8"))
-        reply2 = s.recv(1024)
-        reply2serialized = json.loads(reply2)
-        print(reply2serialized)
+class Client1:
+    def __init__(self):
+        self.clientid = None
+    def connect(self, clientid):
+        self.clientid = clientid
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ServerIP, Port))
+        msgraw = s.recv(1024)
+        msg = msgraw.decode("utf-8")
+        print(msg)
+        if "connection" in msg:
+            reply = {}
+            reply["studentnr"] = "123456"
+            reply["classname"] = "INF2C"
+            reply["clientid"] = self.clientid
+            reply["teamname"] = "Gerrie en Timmie"
+            reply["ip"] = socket.gethostbyname(socket.gethostname())
+            reply["secret"] = None
+            reply["status"] = None
+            replyserialized = json.dumps(reply)
+            s.send(bytes(replyserialized, "utf-8"))
+            reply2 = s.recv(1024)
+            reply2serialized = json.loads(reply2)
+            print(reply2serialized)
 
+c1 = Client1()
+c2 = Client1()
 
+def Main():
+    clientprompt = input("Which client do you want to use? Please only use 1 or 2.    :")
+    print(clientprompt)
+    if clientprompt == "1":
+        clientid = 1
+        c1.connect(clientid)
+    elif clientprompt == "2":
+        clientid = 2
+        c2.connect(clientid)
+    else:
+        print("Wrong client ID. Please try again.")
+        Main()
+
+Main()
 
