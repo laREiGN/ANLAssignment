96f89f0ec17c0830b916e04be6bc5d8102cf9d4c
laREiGN / Girts <43932616+laREiGN@users.noreply.github.com>
2019-10-24 19:38:37 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 import socket
 import sys
 
-# Server IP: 145.24.222.103
-# Port: 8001
+ServerIP = "145.24.222.103"
+Port = 8001
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("145.24.222.103", 8001))
-msg = s.recv(1024)
+s.connect((ServerIP, Port))
+msgraw = s.recv(1024)
+msg = msgraw.decode("utf-8")
 print(msg)
+
